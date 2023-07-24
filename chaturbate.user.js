4f685d6c6d9a3c14c68d52b4166bec62d15fbfa4
DeAlexPesh <DeAlexWin@gmail.com>
2023-07-24 23:36:57 +0500

    Update chaturbate.user.js

--- chaturbate.user.js
+++ chaturbate.user.js
@@ -6,6 +6,8 @@
 // @author       I
 // @match        *://*chaturbate-online.ru/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=chaturbate-online.ru
+// @downloadURL https://raw.githubusercontent.com/DeAlexPesh/tampermonkey/main/chaturbate.user.js
+// @updateURL https://raw.githubusercontent.com/DeAlexPesh/tampermonkey/main/chaturbate.user.js
 // @grant        none
 // ==/UserScript==
 
